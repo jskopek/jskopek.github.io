a52f34352806960017953857ed2870fe1980389e
Jean-Marc Skopek <jskopek@gmail.com>
2018-04-30 01:01:53 -0700

    Reimplemented front page

--- assets/front-page.js
+++ assets/front-page.js
@@ -1,12 +1,8 @@
-/* pick a random background from list of options */
-var backgrounds = [
-    { 'id': 'london-city-center', 'source': 'City Center, London' },
-    { 'id': 'eiffel-tower', 'source': 'Eiffel Tower, Paris' },
-    { 'id': 'samuel-beckett-bridge', 'source': 'Samuel Beckett Bridge, Ireland' },
-];
-var index = Math.floor(Math.random() * backgrounds.length);
-var background = backgrounds[index];
-document.getElementById('background-video').style.backgroundImage = "url(/assets/" + background.id + ".jpg)";
-document.getElementById('background-video').src = "/assets/" + background.id + ".mp4";
-document.getElementById('background-video-source').textContent = background.source;
+// setTimeout(function() {
+//     document.querySelector('.homepage-gradient.one').style.opacity = "0.0";
+//     document.querySelector('.homepage-gradient.two').style.opacity = "1.0";
+// }, 1000);
+// 
+// look into pure pjax library
+// https://github.com/MoOx/pjax
 
